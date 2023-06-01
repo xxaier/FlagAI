641e6641391adf58351229ca0963d1c0217e5d13
xxai.art <xxai.art@gmail.com>
2023-06-01 20:11:15 +0800

    altclip export onnx

---
+++ onnx/export.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+DIR=$(realpath $0) && DIR=${DIR%/*}
+cd $DIR
+set -ex
+
+cmd="export/onnx.sh"
+
+if ! [ -z $1 ]; then
+  cmd="$cmd && $@"
+fi
+
+# ./bash.sh "export MODEL=AltCLIP-XLMR-L && $cmd"
+# ./bash.sh "export MODEL=AltCLIP-XLMR-L-m9 && $cmd"
+./bash.sh $cmd
